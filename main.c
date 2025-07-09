fcb3fb7252501c26af971f167933c952c6d8e9bc
Alexander Popov <lxndrppv@gmail.com>
2025-07-09 18:37:27 +0300

    fix: updated cmakefile

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include "game.c"
+#include "map.h"
 
 int main() {
-    game_loop();
+    // game_loop();
+    load_map_from_file();
     return 0;
 }
--- map.c
+++ map.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 #define MAP_ROW_MAX_WIDTH 100
 
